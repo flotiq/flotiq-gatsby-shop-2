5e51cd618b520420e76b591f3572bf4697bb39ce
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-02-10 12:50:02 +0100

    hero section build

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Button, Header, Image, Paragraph } from 'flotiq-components-react';
+import { Button, Card, Header, Image, Paragraph } from 'flotiq-components-react';
 import { navigate } from 'gatsby';
 
-const Hero = ({ heroImage, headerText, paragraphText, buttonLabel }) => (
-    <div className="flex flex-wrap items-center justify-between">
-        <div className="px-10 xl:pl-32 lg:pr-5 py-16 basis-full lg:basis-5/12 order-2 lg:order-1">
+const Hero = ({ heroImage, headerText, paragraphText, buttonLabel, onClick }) => (
+
+    <Card
+        vertical
+        onClick={onClick}
+        bordered={false}
+        rounded="none"
+        additionalClasses={['mb-4 cursor-pointer mx-auto ']}
+        proportionsForVertical={{
+            body: '2/5',
+            img: '3/5',
+            breakpoint: 'lg',
+        }}
+    >
+        <Card.Body
+            additionalClasses={[
+                'flex flex-col items-start justify-center '
+                + 'order-2 lg:order-1 px-10 xl:pl-32 lg:pr-5 !py-22 xl:!py-28 min-h-1/2vh',
+            ]}
+        >
             <Header
                 text={headerText}
                 additionalClasses={['!font-thin !text-5xl md:!text-6xl lg:!text-7xl font-roboto']}
@@ -18,15 +35,15 @@ const Hero = ({ heroImage, headerText, paragraphText, buttonLabel }) => (
                 variant="info"
                 additionalClasses={['!text-white !font-light !text-2xl !bg-secondary mt-8 hover:opacity-80']}
             />
-        </div>
+        </Card.Body>
         <div
             style={{ backgroundImage: `url('${heroImage}')` }}
-            className="w-full basis-full lg:basis-7/12 order-1 lg:order-2 bg-cover bg-center"
+            className="w-full lg:w-auto order-1 lg:order-2 lg:basis-3/5 bg-cover bg-center"
         >
-            <Image url={heroImage} additionalClasses={['block lg:hidden w-full']} />
+            <Card.Img src={heroImage} additionalClasses={['block lg:hidden w-full']} />
         </div>
 
-    </div>
+    </Card>
 );
 
 export default Hero;
