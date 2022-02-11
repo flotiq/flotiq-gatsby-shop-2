5b3c3aa5db72635ea55820f2c52a3871f38f39fc
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-02-12 00:02:39 +0100

    reviews section build

--- src/templates/index.js
+++ src/templates/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { graphql } from 'gatsby';
 import Layout from '../layouts/layout';
+import ReviewsSection from '../sections/ReviewsSection';
+import Avatar from '../assets/avatar.png';
 
 const IndexPage = ({ data }) => {
     const products = data.allProduct.nodes;
+    const reviews = [
+        { review: 'The best store in our town! plants are always in good condition.',
+            author: 'Joe Jonas' },
+        { review: 'I ordered 3 packs of roses last friday and they all came so fast and fresh',
+            author: 'Kate Smith' },
+        { review: 'The staff was so sweet they helped me decorate my room with plants!',
+            author: 'John Doe' },
+    ];
     return (
         <Layout additionalClass={['bg-white']}>
             <Helmet>
                 <title>Flotiq Gatsby shop starter</title>
             </Helmet>
-            {products.map((product) => (
-                <a href={`/${product.slug}`}><p key={product.id}>{product.name}</p></a>
-            ))}
+            <ReviewsSection headerText="Reviews" reviews={reviews} avatar={Avatar} />
         </Layout>
     );
 };
