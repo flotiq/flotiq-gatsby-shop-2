d4f18f1b62941b380c2b25bfa55ef14026139f57
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-02-11 13:37:05 +0100

    product card build

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Card } from 'flotiq-components-react';
 
-const ProductCard = ({ name, description, price, productImage }) => (
+const ProductCard = ({ name, description, price, productImage, buttonLabel }) => (
     <Card
         bordered={false}
         rounded="none"
@@ -9,22 +9,25 @@ const ProductCard = ({ name, description, price, productImage }) => (
         <Card.Img
             src={productImage}
             alt={name}
+            additionalClasses={['w-full']}
         />
-        <Card.Body additionalClasses={['!px-0']}>
-            <Card.Title additionalClasses={['flex justify-between !font-medium']}>
+        <Card.Body additionalClasses={['!px-0 !pt-3']}>
+            <Card.Title additionalClasses={['flex justify-between !font-medium !text-2xl']}>
                 <span>{name}</span>
                 <span>
                     $
                     {price}
                 </span>
             </Card.Title>
-            <Card.Text additionalClasses={['line-clamp-4']}>
-                {description}
-            </Card.Text>
+            <div
+                className="line-clamp-2 mt-2 mb-3 text-lg font-light"
+                dangerouslySetInnerHTML={{ __html: description }}
+            />
             <Button
-                label="Button"
+                label={buttonLabel}
                 rounded="none"
                 size="sm"
+                additionalClasses={['bg-secondary w-full justify-center text-xl font-light']}
             />
         </Card.Body>
     </Card>
--- src/sections/ProductCards.js
+++ src/sections/ProductCards.js
@@ -4,10 +4,10 @@ import ProductCard from '../components/ProductCard';
 
 const ProductCards = ({ products, additionalClass }) => (
     <div
-        className={['grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-x-10 max-w-7xl mt-6 mb-6 '
+        className={['grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-10 max-w-7xl mt-6 mb-6 '
         + 'mx-auto px-4 py-4 sm:px-6 lg:px-8 xl:px-0', ...additionalClass].join(' ')}
     >
-        {products.slice(0, 3).map((product) => (
+        {products.slice(0, 4).map((product) => (
             <ProductCard
                 key={product.id}
                 onClick={() => { navigate(`/${product.slug}`); }}
@@ -15,6 +15,7 @@ const ProductCards = ({ products, additionalClass }) => (
                 description={product.description}
                 price={product.price}
                 productImage={product.productImage[0] && product.productImage[0].localFile.publicURL}
+                buttonLabel="Add to cart"
             />
         ))}
     </div>
