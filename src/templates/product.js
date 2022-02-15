41f17c184b49e03f8a30115185144171699cada4
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-02-15 17:41:09 +0100

    similar products section added

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../layouts/layout';
+import Products from '../sections/Products';
 
 const ProductTemplate = ({ data }) => {
     const { product } = data;
+    const products = data.allProduct.nodes;
     return (
         <Layout additionalClass={['bg-white']}>
-            <div>Product</div>
+            <Products products={products} additionalClass={['my-5']} headerText="Similar products" />
         </Layout>
     );
 };
@@ -39,6 +41,39 @@ export const pageQuery = graphql`
             productGallery {
                 localFile {
                     publicURL
+                }
+            }
+        }
+        allProduct(sort: {fields: flotiqInternal___createdAt, order: DESC}, limit: 4, filter: {slug: {ne: $slug}}) {
+            nodes {
+                name
+                price
+                slug
+                description
+                id
+                productGallery {
+                    extension
+                    url
+                    width
+                    height
+                    localFile {
+                        publicURL
+                        childImageSharp {
+                            gatsbyImageData(layout: FULL_WIDTH)
+                        }
+                    }
+                }
+                productImage {
+                    extension
+                    url
+                    width
+                    height
+                    localFile {
+                        publicURL
+                        childImageSharp {
+                            gatsbyImageData(layout: FULL_WIDTH)
+                        }
+                    }
                 }
             }
         }
