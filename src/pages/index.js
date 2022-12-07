da5253cdbcd5d0546d3d65fb134f7505e331ffe1
jaceksekula1337 <110597769+jaceksekula1337@users.noreply.github.com>
2022-12-08 00:42:56 +0100

    eslint update

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,104 +10,104 @@ import Products from '../sections/Products';
 import ReviewsSection from '../sections/ReviewsSection';
 
 const IndexPage = () => {
-    const data = useStaticQuery(query);
-    const products = data.allProduct.nodes;
-    const reviews = [
-        {
-            review: 'The best store in our town! plants are always in good condition.',
-            author: 'Joe Jonas',
-        },
-        {
-            review: 'I ordered 3 packs of roses last friday and they all came so fast and fresh',
-            author: 'Kate Smith',
-        },
-        {
-            review: 'The staff was so sweet they helped me decorate my room with plants!',
-            author: 'John Doe',
-        },
-    ];
-    return (
-        <Layout additionalClass={['bg-white']}>
-            <Helmet>
-                <title>{data.site.siteMetadata.title}</title>
-                <meta
-                    name="description"
-                    content={data.site.siteMetadata.description}
-                />
-            </Helmet>
-            <Hero
-                headerText="Best Plants for your home"
-                paragraphText="Check our store and find the most beautiful plant for your home"
-                buttonLabel="All the products"
-                heroImage={data.heroImage}
-            />
-            <BestSellers products={products} additionalClass={['bg-green-gray py-14']} headerText="Best sellers" />
-            <Products products={products} additionalClass={['my-5']} headerText="Products" />
-            <ReviewsSection
-                headerText="Reviews"
-                reviews={reviews}
-                additionalClass={['my-10 lg:my-24']}
-            />
-            <ImageWithText
-                image={ImageWithTextBackground}
-                headerText1="Buy one"
-                headerText2="plant get"
-                headerText3="one for free"
-                buttonLabel="All the products"
-                additionalClass={['']}
-            />
-        </Layout>
-    );
+  const data = useStaticQuery(query);
+  const products = data.allProduct.nodes;
+  const reviews = [
+    {
+      review: 'The best store in our town! plants are always in good condition.',
+      author: 'Joe Jonas',
+    },
+    {
+      review: 'I ordered 3 packs of roses last friday and they all came so fast and fresh',
+      author: 'Kate Smith',
+    },
+    {
+      review: 'The staff was so sweet they helped me decorate my room with plants!',
+      author: 'John Doe',
+    },
+  ];
+  return (
+    <Layout additionalClass={['bg-white']}>
+      <Helmet>
+        <title>{data.site.siteMetadata.title}</title>
+        <meta
+          name="description"
+          content={data.site.siteMetadata.description}
+        />
+      </Helmet>
+      <Hero
+        headerText="Best Plants for your home"
+        paragraphText="Check our store and find the most beautiful plant for your home"
+        buttonLabel="All the products"
+        heroImage={data.heroImage}
+      />
+      <BestSellers products={products} additionalClass={['bg-green-gray py-14']} headerText="Best sellers" />
+      <Products products={products} additionalClass={['my-5']} headerText="Products" />
+      <ReviewsSection
+        headerText="Reviews"
+        reviews={reviews}
+        additionalClass={['my-10 lg:my-24']}
+      />
+      <ImageWithText
+        image={ImageWithTextBackground}
+        headerText1="Buy one"
+        headerText2="plant get"
+        headerText3="one for free"
+        buttonLabel="All the products"
+        additionalClass={['']}
+      />
+    </Layout>
+  );
 };
 
 const query = graphql`
-query indexQuery {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-    allProduct(sort: {flotiqInternal: {createdAt: DESC}}, limit: 4) {
-      nodes {
-        name
-        price
-        slug
-        description
-        id
-        productGallery {
-          extension
-          url
-          width
-          height
-          localFile {
+    query indexQuery {
+        site {
+            siteMetadata {
+                title
+                description
+            }
+        }
+        allProduct(sort: {fields: flotiqInternal___createdAt, order: DESC}, limit: 4) {
+            nodes {
+                name
+                price
+                slug
+                description
+                id
+                productGallery {
+                    extension
+                    url
+                    width
+                    height
+                    localFile {
+                        publicURL
+                        childImageSharp {
+                            gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+                        }
+                    }
+                }
+                productImage {
+                    extension
+                    url
+                    width
+                    height
+                    localFile {
+                        publicURL
+                        childImageSharp {
+                            gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+                        }
+                    }
+                }
+            }
+        }
+        heroImage: file(name: {eq: "hero-bg"}) {
             publicURL
             childImageSharp {
-              gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+                gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
             }
-          }
         }
-        productImage {
-          extension
-          url
-          width
-          height
-          localFile {
-            publicURL
-            childImageSharp {
-              gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
-            }
-          }
-        }
-      }
     }
-    heroImage: file(name: {eq: "hero-bg"}) {
-      publicURL
-      childImageSharp {
-        gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
-      }
-    }
-  }
 `;
 
 export default IndexPage;
